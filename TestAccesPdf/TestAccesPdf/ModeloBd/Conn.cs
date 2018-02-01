891d92c89a57031377d6320078c074af91a26557
Ing.Luis Acosta <albert_zel@hotmail.com>
2018-02-01 09:19:28 -0600

    Add files via upload

---
+++ TestAccesPdf/TestAccesPdf/ModeloBd/Conn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestAccesPdf.ModeloBd
+{
+    public class Conn<T> where T : class
+    {
+
+        public delegate T LoadEntity(string dt);
+        private LoadEntity CreateItem;
+        BDpdfSrcEntities1 Context;
+        public Conn(LoadEntity loadMethod)
+        {
+            Context = new BDpdfSrcEntities1();
+            CreateItem = loadMethod;
+        }
+        public Conn()
+        {
+            Context = new BDpdfSrcEntities1();
+        }
+
+        public IList<T> SelectToList(string SQLSelect)
+        {
+            return Context.Database.SqlQuery<T>(SQLSelect).ToList();
+        }
+
+        public T SelectFirst(string SQLSelect)
+        {
+            try{return Context.Database.SqlQuery<T>(SQLSelect).First();}
+            catch{ return null; }
+        }
+    }
+}
---
+++ TestAccesPdf/TestAccesPdf/ModeloBd/DataBD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAccesPdf.ModeloBd
+{
+    public class DataBD
+    {
+        private DataBD(){}
+
+        public static Users getUser(string User, string pass)
+        {
+            return new Conn<Users>()
+                .SelectFirst("sp_getUser @pUser = " + User + ", @pPassword = " + pass);
+        }
+    }
+}
